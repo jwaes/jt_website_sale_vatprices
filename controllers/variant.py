83c8d066c98449c90707d13c05fd31dff80b6094
jwaes <jaco.waes@gmail.com>
2024-07-24 14:30:20 +0000

    sdf

--- controllers/variant.py
+++ controllers/variant.py
@@ -1,10 +1,14 @@
 from odoo import http
 from odoo.http import request
 from odoo.addons.website_sale.controllers.variant import WebsiteSaleVariantController
+from odoo.tools.misc import get_lang
 
-class WebsiteSaleVatinfoVariantController(WebsiteSaleVariantController):
+_logger = logging.getLogger(__name__)
+
+class WebsiteSaleNowVatVariantController(WebsiteSaleVariantController):
 
     @http.route()
     def get_combination_info_website(self, *args, **kwargs):
         request.update_context(website_sale_vatinfo=True)
+        _logger.info('ENTERING WebsiteSaleNowVatVariantController')
         return super().get_combination_info_website(*args, **kwargs)
